dc538d776ccf8e158c5f4209bd5ce78a8e208692
LeeChunHei <lch19980829@gmail.com>
2018-12-18 16:08:55 +0800

    led documentation added

--- inc/device_driver/led.h
+++ inc/device_driver/led.h
@@ -16,14 +16,25 @@ namespace DeviceDriver{
 class Led{
 public:
 	struct Config{
-		uint8_t id;
-		bool is_enable = true;
-		bool is_active_low = true;
+		uint8_t id;					//id of the led
+		bool is_enable = true;		//State of led when construsted, true->led on, false->led off.
+		bool is_active_low = true;	//True when led need to light up with logic low ouput(Led anode connect to GND), False when led need to light up with logic high output(Led cathode connect to GND).
 	};
 	Led(const Config config);
+	
+	/*
+	 * @brief Set the state of the led
+	 *
+	 * @param active: true->led on, false->led off.
+	 */
 	void Set(bool active){
 		led_gpio.Set(is_active_low ^ active);
 	}
+	/*
+	 * @brief Toggle the state of the led.
+	 *
+	 * @retval final led state, true->led on, false->led off.
+	 */
 	bool Toggle(){
 		return led_gpio.Toggle() ^ is_active_low;
 	}
